f263335addace7e29d6efa03b922863d5620223a
otavioadamis <otavioadamis@gmail.com>
2023-03-20 16:44:52 -0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '282ac0eaccmsh5baebf78b788488p19150djsn0aad3eb7b123',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
