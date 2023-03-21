845734fe20d715d5a46d95424992242f253771b6
otavioadamis <otavioadamis@gmail.com>
2023-03-20 22:35:34 -0300

    show weather

--- src/api.js
+++ src/api.js
@@ -8,3 +8,7 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "cbd5f458d5ba3250aec8b3d6b15b60b0"
+
